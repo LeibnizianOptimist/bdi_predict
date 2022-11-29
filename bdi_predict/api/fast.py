9bf7da78ac5d4a0234850c01495028d3929ae538
Justin Lawes <lawesjustin@gmail.com>
2022-11-29 20:29:12 +0900

    made progress with final api

--- bdi_predict/api/fast.py
+++ bdi_predict/api/fast.py
@@ -1,16 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 import pandas as pd
 import numpy as np
-from bdi_predict.ml_logic.registry import load_model
+from keras.preprocessing.sequence import TimeseriesGenerator
+from sklearn.preprocessing import MinMaxScaler
+
+from bdi_predict.ml_logic.registry import load_model, load_data
 from bdi_predict.ml_logic.params import LOCAL_REGISTRY_PATH
-from datetime import datetime
-import pytz
+from bdi_predict.ml_logic.preprocessor import final_preprocess
 
 
 app = FastAPI()
 
 app.state.model = load_model()
+
+app.state.data = load_data()
 
 app.add_middleware(
     CORSMiddleware,
@@ -21,45 +26,48 @@ app.add_middleware(
 )
 
 @app.get("/predict")
-def predict(X1:float,
-            X2:float,
-            X3:float,
-            X4:float,
-            X5:float,
-            X6:float,
-            X7:float,
-            X8:float,
-            X9:float,
-            X10:float
+def predict(
 ):
     """
-    Type hniting is used to indicate the datatypes expected for the parameters of the functino 
+    Type hinting is used to indicate the datatypes expected for the parameters of the functino 
     FastAPI uses this info in order to hand errors to the developpers providing incompatible parameters.
     FastAPI also provides variables of the expected datatype to use without type hinting we need to manually convert the 
     parameters of the functions which are all recieved as strings.
     """
     
-    X = [X1, X2, X3, X4, X5, X6, X7, X8, X9, X10]
-    X_pred = np.array(X) 
-    X_pred = X_pred.reshape(1, 10)
-    assert X_pred.shape == (1, 10)
-    #Shape should be (1, 10, 2)
+    #Taking in data & model 
+    df = app.state.data
+    df["Date"] = pd.to_datetime(df["Date"])
+    df.set_index("Date", inplace=True)
+    
     model = app.state.model
+    
+    #Preprocess: Obtain prediction as a TimeseriesGenerator and y_true as a np.ndarray
+    output = final_preprocess(df=df)
     
     #The returned value of the model is the log difference between the previous input value and the next value (y_pred). We must utilise
     #We must utlise this predicted value to obtain a more useful number, an aboslute value of the index tomorrow, given the model. 
     
-    y_pred_log_diff = model.predict(X_pred)
-    y_pred_log_diff = float(y_pred_log_diff[0])
+    predict_generator = output[0]
+    assert type(predict_generator) == TimeseriesGenerator
     
-    #WORKING OUT COMMON LOG Y PRED, y_pred_log
-    X10_log10 = np.log10(X10)
-    y_pred_log = y_pred_log_diff + X10_log10
+    X20_log = output[1]
+    assert type(X20_log) == np.float64
+     
+    #Make prediction
+    y_pred_log_diff = model.predict(predict_generator)
+    assert type(y_pred_log_diff) == np.ndarray
+    
+    
+    y_pred_log_diff = float(y_pred_log_diff[0][0])
+    
+
+    y_pred_log = y_pred_log_diff + X20_log
     
     #CONVERTING COMMON LOG y_pred_log to y_pred
     y_pred = 10**y_pred_log
-    
-    
+    y_pred = float(y_pred)
+
     return {
         "BDI PREDICTION 1 DAY INTO THE FUTURE": y_pred
     }
